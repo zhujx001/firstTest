491f01e0fc45864c490fc74a59af1a74bdc386d8
zhujx001 <106715812+zhujx001@users.noreply.github.com>
2024-05-21 11:12:18 +0800

    Update initia.sh

--- initia.sh
+++ initia.sh
@@ -67,7 +67,7 @@ function install_node() {
     # 安装所有二进制文件
     git clone https://github.com/initia-labs/initia
     cd initia
-    git checkout v0.2.15
+  
     make install
     initiad version
 
